a5a4e6def6917cc3ecf1727ca5a05a54b88c3cc2
Rodrigo Barros <brodrigo87@gmail.com>
2025-01-17 18:39:24 -0300

    initial commit

---
+++ app/src/main/java/com/example/tread/activity/introActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tread.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tread.R
+
+class introActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
