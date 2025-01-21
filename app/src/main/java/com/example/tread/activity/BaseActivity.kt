0a915f8a1ce321861335fcf616f8be3782f79bdb
Rodrigo Barros <brodrigo87@gmail.com>
2025-01-21 18:30:51 -0300

    initial commit

---
+++ app/src/main/java/com/example/tread/activity/BaseActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tread.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tread.R
+
+class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
--- app/src/main/java/com/example/tread/activity/introActivity.kt
+++ app/src/main/java/com/example/tread/activity/introActivity.kt
@@ -1,12 +1,21 @@
 package com.example.tread.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tread.MainActivity
 import com.example.tread.R
+import com.example.tread.databinding.ActivityIntroBinding
 
 class introActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIntroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        binding= ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener{
+            startActivity(Intent(this@introActivity,MainActivity::class.java))
+        }
     }
 }
