25d75583c2febf38142bb9612fb7527a3d3ecc71
Rodrigo Barros <brodrigo87@gmail.com>
2025-01-22 16:41:32 -0300

    test

--- app/src/main/java/com/example/tread/activity/BaseActivity.kt
+++ app/src/main/java/com/example/tread/activity/BaseActivity.kt
@@ -2,10 +2,17 @@ package com.example.tread.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import com.example.tread.R
 
-class BaseActivity : AppCompatActivity() {
+open class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
     }
 }
--- app/src/main/java/com/example/tread/activity/introActivity.kt
+++ app/src/main/java/com/example/tread/activity/introActivity.kt
@@ -1,13 +1,11 @@
 package com.example.tread.activity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tread.MainActivity
-import com.example.tread.R
 import com.example.tread.databinding.ActivityIntroBinding
 
-class introActivity : AppCompatActivity() {
+class introActivity : BaseActivity() {
     private lateinit var binding: ActivityIntroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,7 +13,7 @@ class introActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.startBtn.setOnClickListener{
-            startActivity(Intent(this@introActivity,MainActivity::class.java))
+            startActivity(Intent(this@introActivity, MainActivity::class.java))
         }
     }
 }
